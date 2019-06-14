2330bdd7e08400fb24da5666aa984e34178100a7
hongbeomAhn <dks3963@naver.com>
2019-06-15 00:35:17 +0900

    Programmers 짝수와 홀수 add

--- src/main/java/level1/EvenAndOdd/Main.java
+++ src/main/java/level1/EvenAndOdd/Main.java
@@ -3,7 +3,7 @@ package level1.EvenAndOdd;
 public class Main {
 
     public static void main(String[] args) {
-        int num = 15;
+        int num = 16;
 
         Solution sol = new Solution();
         System.out.println(sol.solution(num));
