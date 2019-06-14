6bc2d8b6ad3136ffdcd5a8af88b99e951a6de4f1
hongbeomAhn <dks3963@naver.com>
2019-06-15 00:28:34 +0900

    Programmers X만큼 간격이 있는 N개의 숫자 add

---
+++ src/main/java/level1/NnumberswithXspace/Main.java
@@ -0,0 +1,15 @@
+package level1.NnumberswithXspace;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(x, n)));
+    }
+
+}
