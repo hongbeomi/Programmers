d141e8799c435d104983f89f3170c3bad4937b05
hongbeomAhn <dks3963@naver.com>
2019-06-15 00:35:02 +0900

    Programmers 짝수와 홀수 add

---
+++ src/main/java/level1/EvenAndOdd/Main.java
@@ -0,0 +1,12 @@
+package level1.EvenAndOdd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 15;
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(num));
+    }
+
+}
---
+++ src/main/java/level1/EvenAndOdd/Solution.java
@@ -0,0 +1,16 @@
+package level1.EvenAndOdd;
+
+public class Solution {
+
+    public String solution(int num) {
+        String answer;
+        if(num%2 == 0) {
+            answer = "Even";
+        }
+        else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+
+}
