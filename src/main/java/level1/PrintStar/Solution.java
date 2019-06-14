8775d0febef2303ee12cbdc58397e3e726851013
hongbeomAhn <dks3963@naver.com>
2019-06-15 00:42:45 +0900

    Programmers 직사각형 별찍기 add

---
+++ src/main/java/level1/PrintStar/Solution.java
@@ -0,0 +1,20 @@
+package level1.PrintStar;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0 ; i < b ; i ++) {
+            for (int j = 0 ; j < a ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
