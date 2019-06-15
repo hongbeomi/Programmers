89aab382e4fda5bd2063a5c824b58bc67c75e37e
hongbeomAhn <dks3963@naver.com>
2019-06-15 21:01:04 +0900

    Programmers-팁스타운 2017 예상 대진표, 짝지어 제거하기 add

--- src/main/java/tipstown_2017/예상대진표/Main.java
+++ src/main/java/tipstown_2017/예상대진표/Main.java
@@ -3,7 +3,7 @@ package tipstown_2017.예상대진표;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 4, b = 7, c = 8;
+        int a = 4, b = 6, c = 8;
         Solution sol = new Solution();
 
         System.out.println(sol.solution(c, a, b));
