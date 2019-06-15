08ee95f1d34b8576bf41b6e442ace27c0dc486e4
hongbeomAhn <dks3963@naver.com>
2019-06-15 17:55:27 +0900

    Programmers 하샤드 수 add

--- src/main/java/level1/하샤드수/Main.java
+++ src/main/java/level1/하샤드수/Main.java
@@ -3,7 +3,7 @@ package level1.하샤드수;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 10;
+        int x = 12;
         Solution sol = new Solution();
         System.out.println(sol.solution(x));
     }
