81aaae3945541d9a46ae77587847b74dad2d05eb
hongbeomAhn <dks3963@naver.com>
2019-06-16 20:52:52 +0900

    Programmers level2 124나라의 숫자, 올바른 괄호 add

--- src/main/java/level2/올바른괄호/Main.java
+++ src/main/java/level2/올바른괄호/Main.java
@@ -3,7 +3,7 @@ package level2.올바른괄호;
 public class Main {
 
     public static void main(String[] args) {
-        String s = "(()()()()";
+        String s = "()()()()";
 
         Solution sol = new Solution();
         System.out.println(sol.solution(s));
