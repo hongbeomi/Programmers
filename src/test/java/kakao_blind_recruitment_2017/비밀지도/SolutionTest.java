627ef0b43d4ba9f3718118f4be929ce3ca396b85
hongbeomAhn <dks3963@naver.com>
2019-06-23 03:12:09 +0900

    Programmers Kakao Blind Recruitment - 2017 비밀지도 add

--- src/test/java/kakao_blind_recruitment_2017/비밀지도/SolutionTest.java
+++ src/test/java/kakao_blind_recruitment_2017/비밀지도/SolutionTest.java
@@ -21,11 +21,11 @@ public class SolutionTest {
 
   @Test
   public void solution1() {
-    int n = 5;
+    int n = 6;
     int[] arr1 = {46, 33, 33 ,22, 31, 50};
-    int[] arr2 = {30, 1, 21, 17, 28};
+    int[] arr2 = {27 ,56, 19, 14, 14, 10};
 
-    String[] answer = {"#####","# # #", "### #", "#  ##", "#####"};
+    String[] answer = {"######", "###  #", "##  ##", " #### ", " #####", "### # "};
     Solution sol = new Solution();
 
     Assert.assertEquals(answer, sol.solution(n, arr1, arr2));
