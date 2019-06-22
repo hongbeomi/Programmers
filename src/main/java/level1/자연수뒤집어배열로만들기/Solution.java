f1e4eaa73b0abfcea3e53d8b2d1a6a23013542e8
hongbeomAhn <dks3963@naver.com>
2019-06-22 19:26:14 +0900

    Programmers 자연수 뒤집어 배열로 만들기 add

---
+++ src/main/java/level1/자연수뒤집어배열로만들기/Solution.java
@@ -0,0 +1,13 @@
+package level1.자연수뒤집어배열로만들기;
+
+public class Solution {
+
+  public int[] solution(long n) {
+    int[] answer = {};
+
+
+
+    return answer;
+  }
+
+}
---
+++ src/test/java/level1/자연수뒤집어배열로만들기/SolutionTest.java
@@ -0,0 +1,20 @@
+package level1.자연수뒤집어배열로만들기;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+  @Test
+  public void solution() {
+    long n = 12345;
+    int[] answer = {5, 4, 3, 2, 1};
+
+    Solution sol = new Solution();
+
+    Assert.assertArrayEquals(answer, sol.solution(n));
+
+  }
+}
