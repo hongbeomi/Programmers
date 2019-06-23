262c3674043a98b26fe899345e6a4ca3db1f30fa
hongbeomAhn <dks3963@naver.com>
2019-06-23 15:09:07 +0900

    Programmers 탑 add

--- src/test/java/stack_queue/탑/SolutionTest.java
+++ src/test/java/stack_queue/탑/SolutionTest.java
@@ -1,5 +1,7 @@
 package stack_queue.탑;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +13,8 @@ public class SolutionTest {
     int[] heights = {6, 9, 5, 7, 4};
     Solution sol = new Solution();
     int[] answer = {0, 0, 2, 2, 4};
+
+    Assert.assertThat(answer, CoreMatchers.is(sol.solution(heights)));
 
   }
 }
