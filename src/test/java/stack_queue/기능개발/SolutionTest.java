264379f8a059ef939656dada0a566e15223e85bf
hongbeomAhn <dks3963@naver.com>
2019-06-24 15:54:56 +0900

    Programmers 기능개발 add

--- src/test/java/stack_queue/기능개발/SolutionTest.java
+++ src/test/java/stack_queue/기능개발/SolutionTest.java
@@ -1,15 +1,18 @@
 package stack_queue.기능개발;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class SolutionTest {
 
   @Test
   public void solution() {
-    int[] progresses = {};
+    int[] progresses = {93, 30, 55};
+    int[] speeds = {1, 30, 5};
+    int[] answer = {2, 1};
 
-
+    Solution sol = new Solution();
+    Assert.assertEquals(answer, sol.solution(progresses, speeds));
   }
+
 }
