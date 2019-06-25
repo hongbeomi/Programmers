f9690dc9fe161f780479eefc659d67b033ab0568
hongbeomAhn <dks3963@naver.com>
2019-06-25 16:46:44 +0900

    Programmers 더 맵게 add

--- src/test/java/heap/더맵게/SolutionTest.java
+++ src/test/java/heap/더맵게/SolutionTest.java
@@ -16,4 +16,15 @@ public class SolutionTest {
 
     Assert.assertEquals(2, sol.solution(sco, K));
   }
+
+  @Test
+  public void solution1() {
+    int[] sco = {1};
+    int K = 7;
+
+    Solution sol = new Solution();
+
+    Assert.assertEquals(-1, sol.solution(sco, K));
+  }
+
 }
