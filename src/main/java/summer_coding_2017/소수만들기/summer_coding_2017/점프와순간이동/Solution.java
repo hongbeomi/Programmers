13fdf4a4606a67d242a0fb8a5466574141b2ee26
hongbeomAhn <dks3963@naver.com>
2019-06-25 20:51:13 +0900

    Programmers 점프와 순간이동 add

---
+++ src/main/java/summer_coding_2017/소수만들기/summer_coding_2017/점프와순간이동/Solution.java
@@ -0,0 +1,20 @@
+package summer_coding_2017.소수만들기.summer_coding_2017.점프와순간이동;
+
+public class Solution {
+
+  public int solution(int n) {
+    int answer = 0;
+
+    while (n!=0) {
+      if (n % 2 == 0) {
+        n = n / 2;
+      } else {
+        n = n -1;
+        answer++;
+      }
+    }
+
+    return answer;
+  }
+
+}
---
+++ src/test/java/summer_coding_2017/소수만들기/summer_coding_2017/점프와순간이동/SolutionTest.java
@@ -0,0 +1,12 @@
+package summer_coding_2017.소수만들기.summer_coding_2017.점프와순간이동;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+  @Test
+  public void solution() {
+  }
+}
