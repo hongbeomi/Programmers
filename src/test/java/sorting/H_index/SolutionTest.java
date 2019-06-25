5fae87c1b7b6db98920752d72376a521e8d3fc24
hongbeomAhn <dks3963@naver.com>
2019-06-25 20:19:22 +0900

    Programmers H-index add

--- src/test/java/sorting/H_index/SolutionTest.java
+++ src/test/java/sorting/H_index/SolutionTest.java
@@ -1,5 +1,6 @@
 package sorting.H_index;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,10 @@ public class SolutionTest {
 
   @Test
   public void solution() {
+    int[] citations = {3, 0, 6, 1, 5};
+
+    Solution sol = new Solution();
+    Assert.assertEquals(3, sol.solution(citations));
   }
+
 }
