b50faf735fdb9ef170851f97e6713a963009e4c0
hongbeomAhn <dks3963@naver.com>
2019-06-25 21:03:27 +0900

    Programmers 문자열 내 마음대로 정렬하기 test update

--- src/main/java/level1/문자열내마음대로정렬하기/Solution.java
+++ src/main/java/level1/문자열내마음대로정렬하기/Solution.java
@@ -8,21 +8,18 @@ public class Solution {
     public String[] solution(String[] strings, int n) {
         String[] answer = {};
 
-        Arrays.sort(strings, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.charAt(n) > o2.charAt(n)){
-                    return 1;
-                }
-                else if(o1.charAt(n) == o2.charAt(n)){
-                    return o1.compareTo(o2);
-                }
-
-                else if(o1.charAt(n) < o2.charAt(n)){
-                    return -1;
-                }
-                return 0;
+        Arrays.sort(strings, (o1, o2) -> {
+            if(o1.charAt(n) > o2.charAt(n)){
+                return 1;
             }
+            else if(o1.charAt(n) == o2.charAt(n)){
+                return o1.compareTo(o2);
+            }
+
+            else if(o1.charAt(n) < o2.charAt(n)){
+                return -1;
+            }
+            return 0;
         });
 
         return answer = strings;
