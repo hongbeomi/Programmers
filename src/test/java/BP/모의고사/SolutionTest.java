e0a994d9f6c06e0de5d97cdfec69bc7ad1ddfb4b
hongbeomAhn <dks3963@naver.com>
2019-06-29 20:41:37 +0900

    Programmers 모의고사 add

--- src/test/java/BP/모의고사/SolutionTest.java
+++ src/test/java/BP/모의고사/SolutionTest.java
@@ -1,5 +1,7 @@
 package BP.모의고사;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,6 +11,17 @@ public class SolutionTest {
   @Test
   public void solution() {
     int[] answers = { 1, 2, 3, 4, 5 };
+
+    Solution sol = new Solution();
+    Assert.assertThat(new int[]{1}, CoreMatchers.is(sol.solution(answers)));
+  }
+
+  @Test
+  public void solution1() {
+    int[] answers = { 1, 3, 2, 4, 2 };
+
+    Solution sol = new Solution();
+    Assert.assertThat(new int[]{1, 2, 3}, CoreMatchers.is(sol.solution(answers)));
   }
 
 }
