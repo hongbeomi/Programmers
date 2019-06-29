4c6a3dc01d9c12dfea8232d07fd2c22d49bc29ef
hongbeomAhn <dks3963@naver.com>
2019-06-29 16:06:29 +0900

    Programmers 위장 add

--- src/test/java/hash/위장/SolutionTest.java
+++ src/test/java/hash/위장/SolutionTest.java
@@ -1,14 +1,20 @@
 package hash.위장;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class SolutionTest {
 
   @Test
   public void solution() {
+    String[][] clothes = {
+        {"yellow_hat", "headgear"},
+        {"blue_sunglasses", "eyewear"},
+        {"green_turban", "headgear"}
+    };
 
-
+    Solution sol = new Solution();
+    Assert.assertEquals(5, sol.solution(clothes));
   }
+
 }
