c32c18a5b47d9493c844c1258f6ccbc980740f80
hongbeomAhn <dks3963@naver.com>
2019-06-29 17:36:08 +0900

    Programmers 다리를 지나는 트럭 add

--- src/test/java/stack_queue/다리를지나는트럭/SolutionTest.java
+++ src/test/java/stack_queue/다리를지나는트럭/SolutionTest.java
@@ -1,5 +1,6 @@
 package stack_queue.다리를지나는트럭;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,12 @@ public class SolutionTest {
 
   @Test
   public void solution() {
+    int bridge_length = 2;
+    int weight = 10;
+    int[] truck_weights = {7, 4, 5, 6};
+
+    Solution sol = new Solution();
+    Assert.assertEquals(8, sol.solution(bridge_length, weight, truck_weights));
   }
+
 }
