6347aded41cd80252dfec67b1c7b578c165295dd
hongbeomAhn <dks3963@naver.com>
2019-06-29 19:00:42 +0900

    Programmers 라면공장 add

--- src/test/java/heap/라면공장/SolutionTest.java
+++ src/test/java/heap/라면공장/SolutionTest.java
@@ -1,5 +1,6 @@
 package heap.라면공장;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,7 +9,13 @@ public class SolutionTest {
 
   @Test
   public void solution() {
+    int stock = 4;
+    int[] dates = { 4, 10, 15 };
+    int[] supplies = { 20, 5, 10 };
+    int k = 30;
 
-
+    Solution sol = new Solution();
+    Assert.assertEquals(2, sol.solution(stock, dates, supplies, k));
   }
+
 }
