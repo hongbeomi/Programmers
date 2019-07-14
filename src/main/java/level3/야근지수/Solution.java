b1fdd6037d9e8baa4d1284709c152e24c6e49624
hongbeomAhn <dks3963@naver.com>
2019-07-14 15:13:57 +0900

    Programmers 야근지수 add

---
+++ src/main/java/level3/야근지수/Solution.java
@@ -0,0 +1,35 @@
+package level3.야근지수;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+  public long solution(int n, int[] works) { // sort를 쓰면 시간초과가 나와서 우선순위 큐 사용
+    long answer = 0;
+    PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+    for (int work : works) {
+      queue.add(work);
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (queue.size() == 0) {
+        break;
+      }
+      int poll = queue.poll();
+      poll--;
+
+      if (poll != 0) {
+        queue.add(poll);
+      }
+    }
+
+    while (!queue.isEmpty()) {
+      int poll = queue.poll();
+      answer += (long) Math.pow(poll, 2);
+    }
+    return answer;
+  }
+
+}
---
+++ src/test/java/level3/야근지수/SolutionTest.java
@@ -0,0 +1,27 @@
+package level3.야근지수;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+  @Test
+  public void solution() {
+    int n = 4;
+    int[] works = { 4, 3, 3 };
+
+    Solution sol = new Solution();
+    Assert.assertEquals(12, sol.solution(n, works));
+  }
+
+  @Test
+  public void solution1() {
+    int n = 3;
+    int[] works = { 1, 1 };
+
+    Solution sol = new Solution();
+    Assert.assertEquals(0, sol.solution(n, works));
+  }
+}
