7b102e1dd166ab5b336a87e190ef20720c59ca6d
hongbeomAhn <dks3963@naver.com>
2019-09-16 20:11:03 +0900

    Programmers level1 stream 문자열 내림차순으로 배치하기 add

---
+++ src/main/java/stream_solution/level1/문자열내림차순으로배치하기/Solution.java
@@ -0,0 +1,15 @@
+package stream_solution.level1.문자열내림차순으로배치하기;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+  public String solution(String s) {
+    return Arrays.stream(s.split(""))
+        .sorted(Comparator.reverseOrder())
+        .collect(Collectors.joining());
+  }
+
+}
