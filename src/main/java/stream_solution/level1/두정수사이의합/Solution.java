89d22cf6b9add8f56ea70ae32511b5858c2689f4
hongbeomAhn <dks3963@naver.com>
2019-09-16 18:55:42 +0900

    Programmers level1 stream 두 정수 사이의 합 add

---
+++ src/main/java/stream_solution/level1/두정수사이의합/Solution.java
@@ -0,0 +1,17 @@
+package stream_solution.level1.두정수사이의합;
+
+import java.util.stream.LongStream;
+
+public class Solution {
+
+  public long solution(int a, int b) {
+    if(a == b){
+      return b;
+    }
+
+    return LongStream
+        .rangeClosed(Math.min(a, b), Math.max(a, b))
+        .sum();
+  }
+
+}
