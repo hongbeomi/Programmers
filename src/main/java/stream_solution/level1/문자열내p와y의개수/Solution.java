d754ced55a44488dbc5467ba361635ad046f9d5e
hongbeomAhn <dks3963@naver.com>
2019-09-16 19:58:24 +0900

    Programmers level1 stream 문자열 내 p와 y의 개수 add

---
+++ src/main/java/stream_solution/level1/문자열내p와y의개수/Solution.java
@@ -0,0 +1,23 @@
+package stream_solution.level1.문자열내p와y의개수;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Solution {
+
+  boolean solution(String s) {
+    long countP =  Stream.of(s)
+        .map(a -> s.split(""))
+        .flatMap(Arrays::stream)
+        .filter(a -> a.equalsIgnoreCase("p"))
+        .count();
+
+    long countY = Stream.of(s)
+        .map(b -> s.split(""))
+        .flatMap(Arrays::stream)
+        .filter(b -> b.equalsIgnoreCase("y"))
+        .count();
+    return countP == countY;
+  }
+
+}
