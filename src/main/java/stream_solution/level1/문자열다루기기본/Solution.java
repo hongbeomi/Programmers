98ef9874ae40a7f899336c26629ec4589319f927
hongbeomAhn <dks3963@naver.com>
2019-09-16 20:57:11 +0900

    Programmers level1 stream 문자열 다루기 기본 add

---
+++ src/main/java/stream_solution/level1/문자열다루기기본/Solution.java
@@ -0,0 +1,12 @@
+package stream_solution.level1.문자열다루기기본;
+
+import java.util.Arrays;
+
+public class Solution {
+
+  public boolean solution(String s) {
+    return Arrays.stream(s.split(""))
+        .allMatch(a -> (s.length() == 4 || s.length() == 6) || a.matches("^[0-9]*$"));
+  }
+
+}
