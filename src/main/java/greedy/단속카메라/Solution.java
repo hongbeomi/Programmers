4acce7a19bb39dc769db8c0a63fa21d28a3fb589
hongbeomAhn <dks3963@naver.com>
2019-09-20 01:53:10 +0900

    Programmers greedy 과속카메라 add

--- src/main/java/greedy/단속카메라/Solution.java
+++ src/main/java/greedy/단속카메라/Solution.java
@@ -8,6 +8,7 @@ public class Solution {
   public int solution(int[][] routes) {
     int answer = 0;
     int min = Integer.MIN_VALUE;
+
     Arrays.sort(routes, Comparator.comparingInt(o -> o[1]));
     for (int[] route : routes) {
       if (min < route[0]) {
